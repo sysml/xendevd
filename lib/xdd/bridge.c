d9b0a9dd45b16561654a85d8ccaffa61d756d3d9
Filipe Manco <filipe.manco@neclab.eu>
2015-02-23 19:24:32 +0100

    bridge: add basic bridge management lib

---
+++ lib/xdd/bridge.c
@@ -0,0 +1,54 @@
+#include <xdd/bridge.h>
+
+#include <errno.h>
+#include <net/if.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <linux/sockios.h>
+
+
+static int bridge_if(int op, char* bridge, char* dev)
+{
+	int err;
+	struct ifreq ifr;
+
+	int ifindex = if_nametoindex(dev);
+	if (ifindex == 0) {
+		return ENODEV;
+    }
+
+    int sock_fd = socket(AF_LOCAL, SOCK_STREAM, 0);
+	if (sock_fd < 0) {
+		return errno;
+    }
+
+	strncpy(ifr.ifr_name, bridge, IFNAMSIZ);
+	ifr.ifr_ifindex = ifindex;
+
+	err = ioctl(sock_fd, op, &ifr);
+    if (err < 0) {
+        return errno;
+    }
+
+    err = close(sock_fd);
+    if (err < 0) {
+        return errno;
+    }
+
+    return 0;
+}
+
+int bridge_add_if(char* bridge, char* dev)
+{
+    return bridge_if(SIOCBRADDIF, bridge, dev);
+}
+
+int bridge_rem_if(char* bridge, char* dev)
+{
+    return bridge_if(SIOCBRDELIF, bridge, dev);
+}
+
