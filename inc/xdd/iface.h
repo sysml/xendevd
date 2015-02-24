8e0b7b690166b05741d033db68e1a18f2e5482d1
Filipe Manco <filipe.manco@neclab.eu>
2015-02-24 10:43:04 +0100

    iface: add basic interface management lib

---
+++ inc/xdd/iface.h
@@ -0,0 +1,7 @@
+#ifndef __XDD__IFACE__HH__
+#define __XDD__IFACE__HH__
+
+int iface_set_up(char* dev);
+int iface_set_down(char* dev);
+
+#endif /* __XDD_IFACE_HH__ */
---
+++ lib/xdd/iface.c
@@ -0,0 +1,59 @@
+#include <xdd/iface.h>
+
+#include <errno.h>
+#include <net/if.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <linux/sockios.h>
+
+
+static int iface_flag_set(int flag, char* dev)
+{
+    int err;
+    struct ifreq ifr;
+
+    int sock_fd = socket(AF_LOCAL, SOCK_STREAM, 0);
+	if (sock_fd < 0) {
+		return errno;
+    }
+
+    strncpy(ifr.ifr_name, dev, IFNAMSIZ);
+
+    err = ioctl(sock_fd, SIOCGIFFLAGS, &ifr);
+    if (err < 0) {
+        return errno;
+    }
+
+    if (flag < 0) {
+        flag = -flag;
+        ifr.ifr_flags &= ~flag;
+    } else {
+        ifr.ifr_flags |= flag;
+    }
+
+    err = ioctl(sock_fd, SIOCSIFFLAGS, &ifr);
+    if (err < 0) {
+        return errno;
+    }
+
+    err = close(sock_fd);
+    if (err < 0) {
+        return errno;
+    }
+
+    return 0;
+}
+
+int iface_set_up(char* dev)
+{
+    return iface_flag_set(IFF_UP, dev);
+}
+
+int iface_set_down(char* dev)
+{
+    return iface_flag_set(-IFF_UP, dev);
+}
